11351a8bb75901956370e49b54c6cad1f95ea90b
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2025-02-23 11:10:47 +0000

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import * as React from "react"
+import "./index.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="container">
+      <h1>Hello world!, this is a Gatsby site!</h1>
+      <p>Welcome to your new Gatsby site. Here are some example elements:</p>
+      <button className="example-button">Click Me</button>
+      <button className="example-button">Another Button</button>
+      <div className="example-box">This is a styled box</div>
+    </div>
+  )
 }
